3f1d4fed1190059eac6eb8b5d349433bdb5d25a8
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-16 17:24:44 -0400

    added JS API code and proxy server

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("foo");
+//get quote from API
+
+async function getQuote(){
+    const urlAPI = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jaon';
+    try{
+        const response = await fetch(urlAPI);
+        const data = await response.json();
+        console.log(data);
+
+    }catch(error){
+        console.log('Whoops No Quotes', error);
+    }
+}
+
+
+
+//On Load
+getQuote();
