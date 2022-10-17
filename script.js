1d877af2cb4e7c97655fff2df5099498627a1b29
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-17 01:08:33 -0400

    added heroku proxy server and proxyURL

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //get quote from API
 
 async function getQuote(){
+    const proxyURL = 'https://pure-sierra-31657.herokuapp.com/';
     const urlAPI = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jaon';
     try{
-        const response = await fetch(urlAPI);
+        const response = await fetch(proxyURL, urlAPI);
         const data = await response.json();
         console.log(data);
 
