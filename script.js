f7270a8b717260377a8dad270336d8d582198273
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-17 02:16:02 -0400

    added JSON output to HTML and javascript styling

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 //get quote from API
 
 async function getQuote(){
     /*
-    'https://pure-sierra-31657.herokuapp.com/';
+    'https://cors-anywhere.herokuapp.com/';
+
     */
-    const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+    const proxyURL = 'https://pure-sierra-31657.herokuapp.com/';
     const urlAPI = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
         const response = await fetch(proxyURL + urlAPI);
         const data = await response.json();
-        console.log(data);
 
-        
+        if (data.quoteAuthor ===''){
+            authorText.innerText = "--Unknown"
+        } else {
+            quoteText.innerText = data.quoteText;
+        }
 
+        /* reduce font size for long quotes.*/
+        if (data.quoteText.length > 120){
+            quoteText.classList.add('long-quote');
+        } else {
+            authorText.classList.remove('long-quote');
+        }
+
+        authorText.innerText = data.quoteAuthor;
 
     }catch(error){
         getQuote();
-        console.log('Whoops No Quotes', error);
     }
 }
 
