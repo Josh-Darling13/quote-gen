e1296bc2dabbebb7064ae22b95456412d7442bd8
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-10-17 01:42:25 -0400

    fixed error in API

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 //get quote from API
 
 async function getQuote(){
-    const proxyURL = 'https://pure-sierra-31657.herokuapp.com/';
-    const urlAPI = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jaon';
+    /*
+    'https://pure-sierra-31657.herokuapp.com/';
+    */
+    const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+    const urlAPI = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
-        const response = await fetch(proxyURL, urlAPI);
+        const response = await fetch(proxyURL + urlAPI);
         const data = await response.json();
         console.log(data);
 
+        
+
+
     }catch(error){
+        getQuote();
         console.log('Whoops No Quotes', error);
     }
 }
-
-
 
 //On Load
 getQuote();
